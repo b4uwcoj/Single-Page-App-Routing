8aae8ed4f2ab57085ace4b65f3a9802d2c49f9a1
b4uwcoj <wahyudwi8514@gmail.com>
2020-11-12 09:53:34 +0200

    routing#3

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,7 +3,6 @@ import Posts from './Posts/Posts';
 import './Blog.css';
 import { Route, NavLink, Switch } from 'react-router-dom';
 import NewPost from './NewPost/NewPost';
-import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
     render () {
@@ -13,7 +12,7 @@ class Blog extends Component {
                     <nav>
                         <ul>
                             <li><NavLink 
-                                to="/" 
+                                to="/posts/" 
                                 exact
                                 activeClassName="my-active"
                                 activeStyle={{
@@ -31,9 +30,8 @@ class Blog extends Component {
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} />
                 <Route path="/" render={() => <h1>Home 2</h1>} /> */}
                 <Switch>
-                    <Route path="/" exact component={Posts}/>
-                    <Route path="/:id" exact component={FullPost}/>
                     <Route path="/new-post" component={NewPost}/>
+                    <Route path="/posts" component={Posts}/>
                 </Switch>
             </div>
         );
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -2,7 +2,9 @@ import React, {Component} from 'react';
 // import { Link } from 'react-router-dom';
 import axios from '../../../axios';
 import Post from '../../../components/Post/Post';
+import {Route} from 'react-router-dom';
 import './Posts.css';
+import FullPost from '../FullPost/FullPost';
 
 class Posts extends Component {
     state = {
@@ -29,7 +31,7 @@ class Posts extends Component {
     }
 
     postSelectedHandler = (id) => {
-        this.props.history.push({pathname: '/' + id});
+        this.props.history.push({pathname: '/posts/' + id});
         // this.props.history.push('/' + id);
     }
 
@@ -49,9 +51,12 @@ class Posts extends Component {
             });
         }    
         return (
-            <section className="Posts">
-                {posts}
-            </section>
+            <div>
+                <section className="Posts">
+                    {posts}
+                </section>
+                    <Route path={this.props.match.url + '/:id'} exact component={FullPost}/>
+            </div>
         );
 
     }
